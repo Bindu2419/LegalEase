632e387502f23a658114cb4003c2c0f7bc62ccfd
Bindu2419 <binduchinta0@gmail.com>
2026-09-17 11:09:39 +0530

    Fix API URL and CORS

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,13 @@
-import axios from 'axios';const api=axios.create({baseURL:'http://localhost:5000/api'});api.interceptors.request.use(c=>{const t=localStorage.getItem('le_token');if(t)c.headers.Authorization=`Bearer ${t}`;return c});export default api;
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://legalease-pdqa.onrender.com/api"
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("le_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
